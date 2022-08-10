f8c7c490aa603444ecd4be09ccb1a852bec0e10b
Angel Escobar <escobar@Angels-MacBook-Pro-162.local>
2022-08-10 01:56:42 -0500

    format the readme and had the inputs displayed. Add badge and license url' git add .

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const inquirer = require('inquirer')
 const chalk = require('chalk')
 const fs = require('fs')
+// const { create } = require('domain')
 
 // TODO: Create an array of questions for user input
 // const questions = [];
@@ -20,6 +21,11 @@ inquirer
         },
         {
             type: 'input',
+            message: chalk.blue('What are the installation instructions?'),
+            name : 'instructions'
+        },
+        {
+            type: 'input',
             message: chalk.red('Usage Information'),
             name: 'info',
         },
@@ -30,13 +36,13 @@ inquirer
         },
         {
             type: 'input',
-            message: chalk.blue('What are the resting instructions?'),
-            name : 'instructions'
+            message: chalk.yellow('What are the test Instructions'),
+            name: 'test',
         },
         {
-            type: 'checkbox',
+            type: 'list',
             message: chalk.green('Licenses?'),
-            choices: [ "HTML", "Javascript", "C++", "Python"],
+            choices: [ "MIT", "Javascript", "C++", "Python"],
             name: 'license',
         },
         {
@@ -52,7 +58,100 @@ inquirer
 
     ])
     
-    
+    .then((response) => {
+
+        const readPage = `<div align="center"> 
+
+# ${response.title}\n </div> 
+
+<br> 
+
+${renderLicenseBadge(response.license)}
+
+## About The Project\n 
+
+        ${response.description}\n
+
+<br>
+
+## Table of Contents\n 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contribute](#contributing)
+- [Test](#tests)
+- [Question](#questions)
+
+
+<br>
+
+## Installation\n 
+
+        ${response.instructions}\n 
+
+<br>
+
+## Usage\n 
+
+        ${response.info}\n 
+
+<br>
+
+## License\n
+
+Licensed under the ${renderLicenseLink(response.license)} license.
+
+<br>
+
+## Contributing\n 
+
+            ${response.guidelines}
+
+<br>
+
+## Tests\n 
+
+            ${response.test}
+
+<br>
+
+## Questions\n - ${response.email}\n - ${response.github}\n `
+        
+
+        fs.writeFile('README.md', (readPage), (err) => {
+            err ? console.error(err) : console.log('ReadMe Created!')
+        } )
+
+        function renderLicenseBadge(license) {
+            if(license === "MIT") {
+                return "![License: MIT](https://img.shields.io/badge/License-MIT-orange)";
+            } else {
+                return " "
+            }
+        }
+
+        function renderLicenseLink(license) {
+            if (license === 'MIT' ) {
+                return "[MIT](https://choosealicense.com/licenses/mit/)"
+            
+            }
+        }
+    })
+
+    // TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+
+
+
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
